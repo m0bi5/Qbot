da881fa791d8c652990eefbffd58f5ee8084330c
Mohit Bhasi <mohit.bhasi@yahoo.com>
2016-02-05 13:11:51 +0300

    Update qbot.py

--- qbot.py
+++ qbot.py
@@ -1,15 +1,11 @@
 from selenium import webdriver
-qb=webdriver.PhantomJS(executable_path="C:\\Users\\Bhasi\\Desktop\\Sonus Files\\phantomjs.exe")
+qb=webdriver.PhantomJS(executable_path="C:\\phantomjs.exe")
 qb.get("http://www.namegenerator.biz/random-name-generator.php")
 
 
 password="QwErTy10293"
 def Qbot():
-
-
-    #qbot=webdriver.PhantomJS(executable_path="C:\\Users\\Bhasi\\Desktop\\Sonus Files\\phantomjs.exe")    
-    qbot=webdriver.Chrome(executable_path="C:\\Users\\Bhasi\\Desktop\\Sonus Files\\chromedriver.exe")
-
+    qbot=webdriver.Chrome(executable_path="C:\\chromedriver.exe")
     name=qb.find_element_by_id("rname").text
     qb.find_element_by_id("rnames").click()
     na=name.split(' ')
@@ -21,11 +17,11 @@ def Qbot():
     qbot.find_elements_by_class_name("text")[2].send_keys(password)
     qbot.execute_script("document.getElementsByClassName(\"CaptchaInput\")[0].parentNode.removeChild(document.getElementsByClassName(\"CaptchaInput\")[0])")
     qbot.find_elements_by_class_name("submit_button")[3].click()
-    qbot.get("https://www.quora.com/profile/Mohit-Bhasi-3")
+    qbot.get("___LINK TO YOUR PROFILE___")
     qbot.execute_script("document.getElementsByClassName(\"modal_nux_background\")[0].parentNode.removeChild(document.getElementsByClassName(\"modal_nux_background\")[0])")
     qbot.execute_script("document.getElementsByClassName(\"follow_interests\")[0].parentNode.removeChild(document.getElementsByClassName(\"follow_interests\")[0])")
     qbot.find_elements_by_class_name("TwoStateButton")[0].click()
     qbot.quit()
 
-for x in range(1,1000):
+for x in range(1,1000): #Change numbers depending on number of followers you want.
     Qbot()
